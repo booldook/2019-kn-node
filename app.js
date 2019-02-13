f4d39e5f30992ddefded1c9038c5a60fdf3234e6
Daeseang <eotmdzla@gmail.com>
2019-02-13 16:02:23 +0900

    pug 작업중

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3500;
 
-app.use(express.static('public'));
+app.locals.pretty = true;
+app.use('/', express.static('public'));
+app.use('/assets', express.static('assets'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.get('/page', getQuery);
-app.get('/page/:id', getQuery);
-app.get('/page/:id/:mode', getQuery);
+app.get('/book', getQuery);
+app.get('/book/:id', getQuery);
+app.get('/book/:id/:mode', getQuery);
 
 function getQuery(req, res) {
-  var id = req.query.id;
+  var params = req.params;
   var pageTits = ["MAIN", "PAGE1", "PAGE2", "PAGE3"];
+  if(typeof params.id !== 'undefined') {
+    if(params.id == 'new') {
+      res.render('wr', {title:"글쓰기"});
+    }
+  }
+  else {
   var html = `
-  <ul>
-    <li style="padding:1rem;list-style:none;float:left;width:20%;">
-      <a href="/page?id=0">Main</a></li>
-    <li style="padding:1rem;list-style:none;float:left;width:20%;">
-      <a href="/page?id=1">Page1</a></li>
-    <li style="padding:1rem;list-style:none;float:left;width:20%;">
-      <a href="/page?id=2">Page2</a></li>
-    <li style="padding:1rem;list-style:none;float:left;width:20%;">
-      <a href="/page?id=3">Page3</a></li>
-    <li style="clear:both;list-style:none;"></li>
-  </ul>
-  <div style="text-align:center">
-    <h1>${pageTits[id]}</h1>
-  </div>`; 
-  res.send(html);
+    <ul>
+      <li style="padding:1rem;list-style:none;float:left;width:20%;">
+        <a href="/page?id=0">Main</a></li>
+      <li style="padding:1rem;list-style:none;float:left;width:20%;">
+        <a href="/page?id=1">Page1</a></li>
+      <li style="padding:1rem;list-style:none;float:left;width:20%;">
+        <a href="/page?id=2">Page2</a></li>
+      <li style="padding:1rem;list-style:none;float:left;width:20%;">
+        <a href="/page?id=3">Page3</a></li>
+      <li style="clear:both;list-style:none;"></li>
+    </ul>
+    <div style="text-align:center">
+      <h1></h1>
+    </div>`; 
+    res.send(html);
+  }
 }
 
 /*
@@ -52,10 +66,6 @@ app.get('/page', (req, res) => {
   res.send(html);
 });
 */
-app.get('/book/science', (req, res) => {
-  var html = `<h1>과학카테고리!!!!</h1>`;
-  res.send(html);
-});
 app.get('/info', (req, res) => {
   var now = new Date();
   var html = `
